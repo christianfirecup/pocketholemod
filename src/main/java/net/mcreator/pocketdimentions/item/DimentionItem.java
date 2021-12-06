454174a21ede1bb96eb5164dde922cdce5206069
christianfirecup <christianfirecup@aol.com>
2021-12-06 09:13:31 -0500

    first push

---
+++ src/main/java/net/mcreator/pocketdimentions/item/DimentionItem.java
@@ -0,0 +1,70 @@
+
+package net.mcreator.pocketdimentions.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.Hand;
+import net.minecraft.util.ActionResult;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.pocketdimentions.procedures.DimentionRightClickedInAirProcedure;
+import net.mcreator.pocketdimentions.PocketDimentionsModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@PocketDimentionsModElements.ModElement.Tag
+public class DimentionItem extends PocketDimentionsModElements.ModElement {
+	@ObjectHolder("pocket_dimentions:dimention")
+	public static final Item block = null;
+	public DimentionItem(PocketDimentionsModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MISC).maxStackSize(1).isImmuneToFire().rarity(Rarity.COMMON));
+			setRegistryName("dimention");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public ActionResult<ItemStack> onItemRightClick(World world, PlayerEntity entity, Hand hand) {
+			ActionResult<ItemStack> ar = super.onItemRightClick(world, entity, hand);
+			ItemStack itemstack = ar.getResult();
+			double x = entity.getPosX();
+			double y = entity.getPosY();
+			double z = entity.getPosZ();
+			{
+				Map<String, Object> $_dependencies = new HashMap<>();
+				$_dependencies.put("entity", entity);
+				DimentionRightClickedInAirProcedure.executeProcedure($_dependencies);
+			}
+			return ar;
+		}
+	}
+}
